e4b9e28b2c23feecccb74745e360865c00da432c
Isaykin Vasily <Isaykin.VA@dns-shop.ru>
2023-07-18 13:36:20 +1000

    добавил web

---
+++ lib/main_development.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:injectable/injectable.dart';
+import 'package:todo/application/app.dart';
+import 'package:todo/application/di/di.dart';
+import 'package:todo/application/global.dart';
+import 'package:todo/data/services/todo_synchronizer.dart';
+import 'package:todo/utils/ui.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  settingUpSystemUIOverlay();
+  await configureDependencies(Environment.dev);
+  runZonedGuarded(
+    () async {
+      await getIt<TodoSynchronizer>().synchronize();
+      runApp(const App());
+    },
+    (e, s) => logger.e(e, e, s),
+  );
+}
